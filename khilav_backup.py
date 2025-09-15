c0c5657eeab1aadfd85d7c73a57daef2237e64dd
dishant1123 <dishantshah1417@gmail.com>
2025-09-15 11:46:56 +0530

    ...

---
+++ khilav_backup.py
@@ -0,0 +1,55 @@
+# interpreated languages : easily  inderstand , explain 
+
+"""print("khilav patel")
+print('khilav patel hmt'),print("study in royal")
+"""
+# use : 
+"""
+1. data cleaning : structure  format 
+2. data analysis : charts 
+3. data visualization : dashboard  
+4. ML /AI : machine  train , AI => robot
+5. GUI : graphical user interface 
+6. web development : web app ==> framework ==>django 
+7. game development : game ==> PUBG 
+"""
+
+# code : 1. vs code 2. jpyter notebook 3. google  colab 4. spider 
+
+# use ==> you tube , PUBG ,spoofity ==>use 
+
+# libraries : numpy ==>araay  pandas==> SQL, data clean  matplotlib ==>graphs seaborn==>graphs sklearn ==>ML  
+
+"""
+print("khilav patel")
+print('khilav patel hmt'),print("study in royal")
+print("live in ahm.")
+"""
+
+# comment : 
+
+"""
+1. single  line comment :  #  
+2. multi line comment :""" """  or '''  ''' 
+"""
+
+# comma  create  space in python  : 
+
+# print("my name is khilav","surname is patel")
+
+# escape  character :
+"""
+1. \n : new line 
+2. \t : tab 
+3. \b : backspace 
+4. end = remove the white space.
+"""
+# khilav patel
+print("khilav",end="\b\b")
+print("patel")
+
+# task  :1 
+
+"""
+c:\new_folder\bin\python.exe
+"""
