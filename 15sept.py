657c3f1c56c546d6e1df57999e8feb00ad5a0eaa
dishant1123 <dishantshah1417@gmail.com>
2025-09-15 18:27:09 +0530

    ...

---
+++ 15sept.py
@@ -0,0 +1,63 @@
+# tuple  :immutable    ==>  no changes in tuple .
+
+"""
+t1 =(1,2,3,4,5,6,7,89.78,True,8j)
+
+print(t1)
+print(type(t1))
+ 
+t2=1,2,3,4,5,6,89.78,True,8j
+print(t2)
+print(type(t2))
+
+"""
+# built in function  : len  min max sorted sum 
+
+"""
+t1 =(10,2,3,4,5,6,7,89.78)
+
+print(len(t1))
+print(min(t1))
+print(max(t1))
+print(sorted(t1))  # asc to desc
+print(sum(t1))
+
+print(sorted(t1,reverse=True))  # desc to asc
+"""
+# slicing  : 
+
+"""
+t1 =(10,2,3,4,5,6,7,89.78)
+
+print(t1[2])
+print(t1[2 :5])
+print(t1[-2])
+
+print(t1[1 :5 :2])
+print(t1[ : :2])
+print(t1[ : :-1])
+"""
+
+"""
+t1[2] ="aashta"
+print(t1)   # error  :  bcz of  tuple is  immutable . 
+"""
+
+# method  :  count  , index 
+
+"""
+t1 =(10,2,3,4,5,6,7,89.78,10)
+
+print(t1.count(10))
+print(t1.index(10))
+print(t1.index(10,1,20))
+"""
+
+# convert  : 
+"""
+task  :1 
+
+input  : t1=(10,2,3,4,5,6,7,89.78,10)
+output  : t1=(10,2,3,4,5,6,7,89.78,10,"ved")
+"""
+
