f7ea42eedc8ff8af285ebaa6f085d023b287944d
dishant1123 <dishantshah1417@gmail.com>
2025-09-22 18:20:56 +0530

    .....

---
+++ 22sept.py
@@ -0,0 +1,53 @@
+# string  : immutable  sequences of character  ==> not  changes  in string. 
+
+"""
+s1 = "my name is ved."
+print(s1)
+print(type(s1))
+"""
+
+# built in function  :  len  min max sorted sum  
+
+"""
+s1 = "my name is ved."
+print(len(s1))
+print(min(s1))
+print(max(s1))
+print(sorted(s1))
+"""
+# slicing  : 
+
+s1 ="my name is ved."
+
+print(s1[2])
+print(s1[3])
+print(s1[2 :5])
+print(s1[-3])
+
+print(s1[ -2 : -5 ])
+print(s1[ -5 : -2 ]) # no  output 
+print(s1[ -2 : -5 :-1 ])
+
+print(s1[1 :8 :2])
+print(s1[  :  :2])
+print(s1[  :  :-2])
+print(s1[  :  :-1])
+
+#            -5 -4  -3 -2 -1  0   1 2 3 4 5 6  
+
+"""
+task  :1 
+
+input  :  dishant dipkkumar shah 
+ouput :  d.d.shah 
+
+task :2 
+ask user to enter the two  stiring and  swap  the  first  threee character. 
+
+input  1 :color 
+input  2: full 
+
+output 1:  fulor 
+output 2:  coll  
+ 
+"""
