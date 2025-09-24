1c32c2ee174ff45535d1065e9c46d525824f0eef
dishant1123 <dishantshah1417@gmail.com>
2025-09-24 18:37:42 +0530

    ....

---
+++ 24sept.py
@@ -0,0 +1,82 @@
+# index ,  r index ,find  , r find :  
+
+s1 ="my name is ved."
+
+"""
+print(s1.index("m"))
+print(s1.index("name"))
+print(s1.index("ame"))
+print(s1.index("e"))
+print(s1.index("e",7,20))
+
+print(s1.find("m"))
+print(s1.find("name"))
+print(s1.find("ame"))
+print(s1.find("e"))
+print(s1.find("e",7,20))
+"""
+s1 ="my name is the ved patel."
+
+"""print(s1.rindex("e"))
+print(s1.rindex("e",4,23))
+print(s1.rindex("a"))
+
+print(s1.rfind("e"))
+print(s1.rfind("e",4,23))
+print(s1.rfind("a"))
+
+"""
+
+# split  , r split  ,  partition  , rpartition  : 
+s1 ="my name is the ved patel."
+
+"""print(s1.split())
+print(s1.split("a"))
+print(s1.split("is"))
+"""
+"""print(s1.rsplit())
+print(s1.rsplit("a"))
+"""
+# hw  ==>  what is  diff  between  split  and  rsplit  ?
+
+"""print(s1.partition("a"))  # 3 part divide  
+print(s1.partition("is"))  
+
+print(s1.rpartition("a")) 
+print(s1.rpartition("e"))
+""" 
+
+# replace : 
+
+"""s1 ="my name is the ved and the patel."
+
+print(s1.replace("ved","harix"))
+print(s1.replace(" ",""))
+print(s1.replace("the"," "))
+print(s1.replace("the"," ",2))
+"""
+# task  :1 
+"""
+input  : i am going to goa next month. 
+output : first  o  index number : 6 
+         second  o  index number :12
+         third  o  index number : 15 
+         fourth  o  index number :24  
+
+"""
+# task  :2 
+"""
+second r repalce  with "@". 
+input  : restart 
+output : resta@t 
+"""
+
+#task :3 
+"""
+ask user to enter the  string  and  replace the first space and last space with "_". 
+
+input  :  my name is ved. 
+output : my_name is_ved. 
+"""
+
+
