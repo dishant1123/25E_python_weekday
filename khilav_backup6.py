783352cd478cef128abb4fac637861543b4bce15
dishant1123 <dishantshah1417@gmail.com>
2025-09-25 11:31:08 +0530

    ....

---
+++ khilav_backup6.py
@@ -0,0 +1,32 @@
+# pattern : 
+"""
+1.          2.          3.          4.          5.          6. 
+ 
+* * * * *   *          * * * * *    1           5 4 3 2 1   1 2 3 4 5 
+* * * * *   * *        * * * *      1 2         5 4 3 2     2 3 4 5
+* * * * *   * * *      * * *        1 2 3       5 4 3       3 4 5
+* * * * *   * * * *    * *          1 2 3 4     5 4         4 5
+* * * * *   * * * * *  *            1 2 3 4 5   5           5
+"""
+
+#1 : 
+
+"""
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print()
+"""
+#2 : 
+"""for i in range(1,6):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+"""
+
+#3 :
+for i in range(1,6):
+    for j in range(5,i-1,-1):
+        print("*",end=" ")
+    print()
+ 
