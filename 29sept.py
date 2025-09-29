7430cff2598d132597eab26ec24e0c533ce9a9d3
dishant1123 <dishantshah1417@gmail.com>
2025-09-29 18:19:30 +0530

    string

---
+++ 29sept.py
@@ -0,0 +1,43 @@
+# count  : 
+
+s1="my name is ved patel."
+
+"""print(s1.count("e"))
+print(s1.count("e",10,20))
+
+print(s1.startswith("my"))
+print(s1.endswith("."))
+print(s1.endswith("patel."))
+
+"""
+"""l1=["my","name","is","ved","patel","."]
+# my name is ved patel. 
+s2 =" ".join(l1)
+print(s2)
+"""
+s2="happydiwalI20oct"
+
+"""print(s2.isalpha())
+print(s2.isalnum())
+"""
+"""s3="1234"
+print(s3.isdigit())
+"""
+"""s4="٠١٢٣٤٥" 
+print(s4.isdecimal())
+"""
+"""s5="MY NAME IS VED PATEL."
+print(s5.islower())
+print(s5.isupper())
+"""
+
+# task  : 1 
+"""
+Write a python program that take one input string and in output count the no of words,
+Find No of letters in String,Find the longest word in the String.
+For Example:-
+Input:-This is the python program
+Output:-No of Words=5
+	    No of letters=26(including whitespace)
+	    Longest Word=program
+"""
