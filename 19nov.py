cf46efc95107659d474fed67b6ff47bfcab4ff7a
dishant1123 <dishantshah1417@gmail.com>
2025-11-19 18:50:33 +0530

    ....

--- 19nov.py
+++ 19nov.py
@@ -23,7 +23,7 @@ B.display() # b method
 """
 
 # constructor  : 
-class person :
+"""class person :
     def __init__(self):
         self.name ="krish"
         self.age =20
@@ -50,3 +50,40 @@ s.age =19
 s.city ="amreli"
 print("after  updat values :\n")
 s.show()
+"""
+
+# multiple inheritance  : 
+"""
+a class can inherit from  more than one parent class 
+
+class a : 
+class b: 
+class c(a,b):
+"""
+
+class grandparent :
+    def __init__(self):
+        self.name = "suresh"
+        self.age = 70 
+class parent :
+    def __init__(self):
+        self.father = "mahesh"
+        self.mother ="meena"
+class child(grandparent,parent):
+    def __init__(self,child_name):
+        grandparent.__init__(self)  # gp const call 
+        parent.__init__(self)  # p const call
+        self.child_name = child_name
+    
+    def show(self):
+        print("FAMILY DETAILS")
+        print("grandparent name  is  :",self.name)
+        print("grandparent age  is  :",self.age)
+        print("parent father  is  :",self.father)
+        print("parent mother  is  :",self.mother)
+        print("child name  is  :",self.child_name)
+
+c=child("vedika")
+c.show()
+
+# MRO  :  method resolution order
