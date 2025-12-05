1c4ba0157043d430f558081b763e29ca08495332
dishant1123 <dishantshah1417@gmail.com>
2025-12-05 18:20:00 +0530

    ....

---
+++ 5dec.py
@@ -0,0 +1,39 @@
+# encapsulation  : 
+"""
+get method  : 
+set method  :
+
+private member   : 
+
+"""
+
+"""class student :
+    def __init__(self):
+        self.__name ="vatsal" 
+        self.__age =20
+    
+    def set_age(self,new_age):
+        self.__age = new_age
+    
+    def display(self):
+        print("name :",self.__name)
+        print("age :",self.__age)
+
+s=student()
+s.display()
+print("using  set method ")
+s.set_age(23)
+s.display()
+"""
+
+# encap  + inherit  : 
+"""
+class student : 
+    const ==> name age address 
+    private variable  
+    
+    func ==> name age add 
+    change  ==>1 set 
+
+class emp (student):
+"""
