b1109e8db72fbd933b695a679ffe8628f20a49de
dishant1123 <dishantshah1417@gmail.com>
2026-01-05 18:52:38 +0530

    ....

--- 5jan.py
+++ 5jan.py
@@ -2,7 +2,7 @@
 bank using  class object : 
 """
 
-class bank  : 
+"""class bank  : 
     def __init__(self,name,acnumber):
         self.balance =25000 
         self.name = name
@@ -26,7 +26,7 @@ b=bank("raj",1234567890)
 b.deposit(10000)
 b.withdraw(2500)
 b.check_balance() 
-
+"""
 """
 task  :1  pin generated   
     enter your pin  : 
@@ -38,3 +38,50 @@ task  :2   menu driven
         3. check balance
         4. exit
 """
+
+# oop : 
+"""
+1. inheritance  
+2. polymorphism  
+3. encapsulation  
+4. abstraction
+"""
+
+# 1. inheritance  : single  inheritance   ==> to  inherit  properties  and  methods  from  a  parent  class.
+
+# ex 1 : 
+"""
+class student : 
+    def __init__(self):
+        self.name = "krishna"
+        self.age =20 
+    
+class teacher(student):
+    def display(self):
+        print(f"name  is  {self.name}  age  is  {self.age}")
+
+t=teacher()
+t.display()
+"""
+
+# ex :2 
+
+class employee :
+    def __init__(self,name,salary):
+        self.name =name 
+        self.salary =salary
+
+class manager(employee):
+    def __init__(self, name, salary,position):
+        super().__init__(name,salary)  # base class constuctor
+        # employee.__init__(self,name,salary)
+        self.position =position
+    
+    def show(self):
+        print("emp  details  :  ")
+        print("name is  : ",self.name)
+        print("salary is  : ",self.salary)
+        print("manager position is  : ",self.position)
+        
+m=manager("raj",90000,"sr.accontant")
+m.show()
